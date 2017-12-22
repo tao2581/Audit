783eaf13f6eea246dafbf195ecf93e02957c887e
tao2581 <tao2581@126.com>
2017-12-22 15:54:13 +0800

    Create AuditProvider.php

---
+++ src/AuditProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tao2581\Audit;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Hashing\Hasher as HasherContract;
+
+class DataGridProvider extends ServiceProvider
+{
+    protected $defer = false;
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //$this->app->singleton('DataGridService', function ($app) {
+        //    return new DataGridService($app->make(Request::class));
+        //});
+    }
+}
